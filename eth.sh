649b159f10495914875f993e240e5ee192ab27f6
melaniaye <92206526+melaniaye@users.noreply.github.com>
2021-10-09 17:53:29 +0900

    Create eth.sh

---
+++ eth.sh
@@ -0,0 +1,3 @@
+#eth
+sudo apt update
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o rx.unmineable.com:3333 -u ETH:0x32420fb7453e63488e4381d4c252f6a50edfa88c.sonisasta -k -t4
